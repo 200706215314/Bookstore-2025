d3ccedfc13fe76bf1cffba2dcfebda1fd88a4950
wangwenxuan <200706215314@sjtu.edu.cn>
2025-12-05 18:44:44 +0800

    Complete the first draft of the requirements analysis document

---
+++ Blocklist.h
@@ -0,0 +1,8 @@
+//
+// Created by lenovo on 2025/12/4.
+//
+
+#ifndef BOOKSTORE_2025_BLOCKLIST_H
+#define BOOKSTORE_2025_BLOCKLIST_H
+
+#endif //BOOKSTORE_2025_BLOCKLIST_H
---
+++ Blocknode.h
@@ -0,0 +1,8 @@
+//
+// Created by lenovo on 2025/12/4.
+//
+
+#ifndef BOOKSTORE_2025_BLOCKNODE_H
+#define BOOKSTORE_2025_BLOCKNODE_H
+
+#endif //BOOKSTORE_2025_BLOCKNODE_H
