0a9de3a0775f2c1236ec9a067c3fde4bec573be0
wangwenxuan <200706215314@sjtu.edu.cn>
2025-12-20 14:36:34 +0800

    select

--- src/bookstore.cpp
+++ src/bookstore.cpp
@@ -266,7 +266,7 @@ bool Bookstore::handleBookCommand(const std::vector<std::string>& tokens) {
             if (!parseShowCommand(tokens, type, value)) return false;
             return bookSystem.showBooks(type, value);
         } else if (command == "buy") {
-            exit(1);
+            // exit(1);
             if (tokens.size() != 3) return false;
             long long quantity = std::stoll(tokens[2]);
             double total = 0.0;
@@ -276,7 +276,7 @@ bool Bookstore::handleBookCommand(const std::vector<std::string>& tokens) {
             }
             return success;
         } else if (command == "select") {
-            // exit(1);
+            exit(1);
             // std::cerr << "test1  " << tokens.size()  << std::endl;
             if (tokens.size() != 2) {
                 return false;
